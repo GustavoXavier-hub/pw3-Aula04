a00fd3383cf33226dc8eca3c0c31d517a98d54ac
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2019-08-16 20:18:21 -0300

    Criacao de classes

---
+++ app.ts
@@ -0,0 +1,16 @@
+class Aluno{
+    
+    constructor(public nome:string){}
+
+  exibir(){
+      console.log("Nome:"+this.nome )
+  }
+
+}
+
+
+
+
+let valter =new Aluno("Valter Costa") 
+valter.exibir()
+
